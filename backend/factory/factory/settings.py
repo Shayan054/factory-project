67afe1e8fe4b47639dde3dbcf3dc1027ec743812
Shayan Abdullah <shayanabdullah89@gmail.com>
2026-06-26 23:19:36 -0700

    static files setting changed

--- backend/factory/factory/settings.py
+++ backend/factory/factory/settings.py
@@ -163,7 +163,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -195,5 +197,3 @@ SIMPLE_JWT = {
     'USER_ID_FIELD': 'employee_id',
     'USER_ID_CLAIM': 'user_id',
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
